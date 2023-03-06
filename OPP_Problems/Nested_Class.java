888983326635c5fd176970aef344df5b959d66f7
Mahbub2001 <imahbubturza@gmail.com>
2023-03-06 17:51:00 +0600

    update

---
+++ OPP_Problems/Nested_Class.java
@@ -0,0 +1,37 @@
+//Without static class
+class Outer {
+    Outer() {
+        System.out.println("hello world outside");
+    }
+
+    class Inner {
+        Inner() {
+            System.out.println("hello world inside");
+        }
+    }
+}
+
+public class Nested_Class {
+    public static void main(String[] args) {
+        Outer h = new Outer();
+        Outer.Inner h2 = h.new Inner();
+    }
+}
+
+//With static class
+// class Outer{
+//     Outer(){
+//         System.out.println("hello world outside");
+//     }
+//     static class Inner{
+//         Inner(){
+//             System.out.println("hello world inside");
+//         }
+//     }
+// }
+// public class Nested_Class {
+//     public static void main(String args[]) {
+//         Outer.Inner obj2 = new Outer.Inner();
+
+//     }
+// }
